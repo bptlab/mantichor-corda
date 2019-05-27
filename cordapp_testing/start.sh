b57774e969211870ffa23a1cd895ed7c303dd09c
Lisa Ihde <lisaihde@web.de>
2019-05-28 01:25:09 +0200

    update start.sh

--- cordapp_testing/start.sh
+++ cordapp_testing/start.sh
@@ -1,9 +1,10 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [ "$OSTYPE" == "darwin"* ]
+then
     ./gradlew build
     ./gradlew deployNodes
     workflows-kotlin/build/nodes/runnodes
 else
-    gradlew.bat build
-    gradlew.bat deployNodes
+	./gradlew.bat build
+    ./gradlew.bat deployNodes
     workflows-kotlin/build/nodes/runnodes.bat
 fi
