8066cd01a128f524e1820bb0b800956616deb87a
Jonas Bounama <jobounama@hotmail.de>
2019-05-27 23:11:47 +0200

    multiplatform

--- cordapp_testing/start.sh
+++ cordapp_testing/start.sh
@@ -1,3 +1,9 @@
-./gradlew build
-./gradlew deployNodes
-workflows-kotlin/build/nodes/runnodes
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ./gradlew build
+    ./gradlew deployNodes
+    workflows-kotlin/build/nodes/runnodes
+else
+    gradlew.bat build
+    gradlew.bat deployNodes
+    workflows-kotlin/build/nodes/runnodes.bat
+fi
