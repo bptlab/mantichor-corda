3eca25329cf921a47407e8c83cd8f8bb63aa30bd
Jonas Bounama <jobounama@hotmail.de>
2019-06-30 01:27:03 +0200

    added adapter primitve Server

---
+++ adapterServer/deploy.sh
@@ -0,0 +1,8 @@
+java -jar XmlReader.jar
+cd ../cordapp_Side0f05a461e7046c68790a3249a304714
+chmod +x gradlew
+./gradlew build
+./gradlew deployNodes 
+./workflows-kotlin/build/nodes/runnodes
+./workflows-kotlin/build/nodes/runnodes
+./workflows-kotlin/build/nodes/runnodes
---
+++ adapterServer/index.js
@@ -0,0 +1,50 @@
+/* eslint-disable require-jsdoc */
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const WebSocketServer = require('websocket').server;
+const connections = new Set();
+const exec = require('child_process').exec;
+
+const server = http.createServer((request, response) => {
+  const filePath = __dirname + ((request.url === '/') ? '/index.html' :
+      path.normalize('/static/' + decodeURI(request.url)));
+  switch(request.url.split('?')[0]) {
+    case '/choreographies':
+      let body = '';
+      request.on('data', chunk => {
+          body += chunk.toString(); // convert Buffer to string
+      });
+      request.on('end', () => {
+          //console.log(body);
+          const requestJson = JSON.parse(body);
+          exec('sh deploy.sh');
+
+      });
+      contentType = 'application/json';
+      break;
+  }
+  response.writeHead(200, {
+    'Content-Type': contentType
+  });
+  response.write('Choreographie has been deployed successfully');
+  response.end();
+}).on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+}).listen(8080);
+
+
+wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+wsServer.on('request', (request) => {
+  const connection = request.accept();
+  connections.add(connection);
+  console.log((new Date()) + ' Connection accepted.');
+  connection.on('close', (reasonCode, description) => {
+    console.log((new Date()) + ' Peer ' +
+        connection.remoteAddress + ' disconnected.');
+    connections.delete(connection);
+  });
+});
